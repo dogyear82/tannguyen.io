d121f33d16d1f935fa314221864e701f66651aba
thnguyen00 <tannguyen@Allcovered.com>
2025-07-24 18:35:42 +0900

    styled page title component

--- src/lib/components/PageTitle.svelte
+++ src/lib/components/PageTitle.svelte
@@ -6,8 +6,13 @@
 
 <style>
     .title {
-        color: #000;
+        color: #373737;
         font-size: 150px;
         font-weight: 700;
+        text-shadow:
+            -1px -1px 0 white,
+            1px -1px 0 white,
+            -1px  1px 0 white,
+            1px  1px 0 white;
     }
 </style>
